56b60530ad3ca0921e256c2ea5491b9f348124d0
canevous regardepas <canevorepa@gmail.com>
2016-03-21 19:41:42 +0100

    Commentaire

--- js/src/iteratorDisplayer.js
+++ js/src/iteratorDisplayer.js
@@ -10,7 +10,6 @@ iteratorDisplayer = (function () {
                     throw new Error("A coder");
                 };
             }
-            
         },
 
         main : function() {
@@ -20,7 +19,6 @@ iteratorDisplayer = (function () {
 
             var outputPanel = iteratorDisplayer.outputPanel.create();
             var generatorSelectorPanel = iteratorDisplayer.generatorSelectorPanel.create(generators.generatorsNamesToArray());
-
             var subPanels = [
                 generatorSelectorPanel,
                 iteratorDisplayer.buttonsPanel.create(generators),
@@ -28,10 +26,15 @@ iteratorDisplayer = (function () {
             ];
 
             // Le panneau d'affichage s'inscrit auprès de tous les générateurs.
+            // Il affichera la valeur générée à chaque génération de valeur.
             generators.eachElement(function(generator) {
                 iteratorDisplayer.registerListener(generator, outputPanel);
             });
 
+            // Le liste des générateurs s'inscrit auprès de sélecteur de générateurs
+            // pour se tenir informé du nom du prochain générateur active à qui elle
+            // demandera de débiter des valeurs lors du prochain appuie sur le bouton
+            // "commencer".
             iteratorDisplayer.registerListener(generatorSelectorPanel, generators);
 
             var mainPanel = iteratorDisplayer.mainPanel.create();
